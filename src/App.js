1d27bd362d2bdb5c2978ecd3b7faba721d273168
Tucker McCoy <tuckermmccoy@gmail.com>
2019-11-22 08:51:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You have sucessfully created an Azure DevOps pipeline for a React application!
         </p>
         <a
           className="App-link"
